b421b529f61963d1cb5ed3d69e0ac72cc1742e1a
Antonio <antoniorr@correo.ugr.es>
2022-08-29 20:58:49 +0200

    error in exercise 2 corrected

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,4 +2,4 @@ numero1 = 5
 numero2 = 3
 suma = numero1 + numero2
 cadena_numerica = str(suma)
-print("El tipo de dato de cadena_numerica es: " + type(cadena_numerica) + " y el valor asociado es: " + cadena_numerica)
+print("El tipo de dato de cadena_numerica es: " + str(type(cadena_numerica)) + " y el valor asociado es: " + cadena_numerica)
