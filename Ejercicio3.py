41debb4947aa075f9f80a94f433f12c1b933d485
Antonio <antoniorr@correo.ugr.es>
2022-08-29 22:17:55 +0200

    Exercise 3

---
+++ Ejercicio3.py
@@ -0,0 +1,6 @@
+nombre = "Antonio"
+edad = 18
+print(f"El alumno {nombre}, tiene {edad}")
+print("El alumno {} tiene {}".format(nombre, edad))
+print("El alumno {n} tiene {e}".format(n=nombre, e=edad))
+print("El alumno " + nombre + " tiene " + str(edad))
