698d9c75a90f6d3e2f1dcee03b90c07c24f1dd59
Antonio <antoniorr@correo.ugr.es>
2022-08-29 20:55:18 +0200

    exercise 1 and 2

---
+++ Ejercicio1.py
@@ -0,0 +1,9 @@
+numero1 = 5
+numero2 = 6.5
+numero3 = 7
+print("numero1 es de typo: " + str(type(numero1)) + " con valor: " + str(numero1))
+print("numero2 es de typo: " + str(type(numero2)) + " con valor: " + str(numero2))
+print("numero3 es de typo: " + str(type(numero3)) + " con valor: " + str(numero3))
+
+suma = numero1 + numero2 + numero3
+print("suma es de typo: " + str(type(suma)) + " con valor: " + str(suma))
---
+++ Ejercicio2.py
@@ -0,0 +1,5 @@
+numero1 = 5
+numero2 = 3
+suma = numero1 + numero2
+cadena_numerica = str(suma)
+print("El tipo de dato de cadena_numerica es: " + type(cadena_numerica) + " y el valor asociado es: " + cadena_numerica)
